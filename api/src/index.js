652adfaa1766e011219e25c10f7de8e880049488
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2021-03-09 08:46:47 -0500

    fixed the endpoint

--- api/src/index.js
+++ api/src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api/conditions', conditionsRouters);
+app.use('/api/condition', conditionsRouters);
 
 app.listen(5000, ()=> console.log('app listening on port 5000'));
 
